ed6e465fdc417c6df25c452dc1de3f2611d4231d
Ravi Pradip <ravipradip@gmail.com>
2025-11-16 15:09:06 +0100

    Update Example_2_internal_timed_acquisiton.py

--- Example_2_internal_timed_acquisiton.py
+++ Example_2_internal_timed_acquisiton.py
@@ -1,31 +1,36 @@
 from coredaq_py_api import CoreDAQ
 import time
 
-daq = CoreDAQ("/dev/tty.usbmodem2062346055301") # Set your CoreDAQ port here
+daq = CoreDAQ("/dev/tty.usbmodem2065344D55301") # Set your CoreDAQ port here
 
 print("Device:", daq.idn())
 
 # Streaming capture
-daq.set_oversampling(2) # At 100 Ksps , max oversampling is 1 , At 50 Ksps : 2 , etc..
-daq.set_freq(50_000)
-##No External Trigger
+
+daq.set_freq(50_000) 
+
+# Sampling rate
+
+#No External Trigger
 daq.acq_arm(100_000)
+
 daq.acq_start()
+
 start=time.time()
+
 while not daq.is_data_ready():
     time.sleep(0.1)
+
 end = time.time()
 print(f"Acquired 100_000 frames in {end-start:.2f} seconds")
-# Bulk transfer raw data → mV arrays
-ch = daq.transfer_frames_mv(100_000)
 
-print("Ch1 first 5 samples (mV):", ch[0][:5])
-print("Ch2 first 5 samples (mV):", ch[1][:5])
-print("Ch3 first 5 samples (mV):", ch[2][:5])
-print("Ch4 first 5 samples (mV):", ch[3][:5])
+# Bulk transfer raw data → Watt arrays
+ch = daq.transfer_frames_W(100_000)
 
-
-
+print("Ch1 first 5 samples (W):", ch[0][:5])
+print("Ch2 first 5 samples (W):", ch[1][:5])
+print("Ch3 first 5 samples (W):", ch[2][:5])
+print("Ch4 first 5 samples (W):", ch[3][:5])
 
 
 daq.close()  # Close the connection cleanly
